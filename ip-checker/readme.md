bfeb8408109cb059656dbdeca11d4ef0c03d10f3
tyz <tyz@1token.trade>
2023-09-30 12:53:44 +0800

    update

---
+++ ip-checker/readme.md
@@ -0,0 +1,4 @@
+Write a vue3 based simple project.
+
+Main Goal: It fetched IP from different source like ip138 and ipinfo.io and api.ipify.org etc.
+and display ip address in the html.
--- ip-checker/script.js
+++ ip-checker/script.js
@@ -1,41 +1,104 @@
-// API列表
-const apiList = [
-    'https://api.ipify.org?format=json',
-    'https://api64.ipify.org?format=json',
-    // 添加其他3个API
-  ];
-  
-  const ipListElement = document.getElementById('ip-list');
-  const loadingElement = document.getElementById('loading');
-  
-  // 页面加载时自动执行
-  window.onload = function() {
-    // 显示加载指示器
-    loadingElement.style.display = 'block';
-  
-    // 使用Promise.all等待所有API响应
-    Promise.all(apiList.map(url => fetch(url)
-      .then(response => response.json())
-      .catch(error => ({ error }))
-    ))
-    .then(results => {
-      // 隐藏加载指示器
-      loadingElement.style.display = 'none';
-  
-      results.forEach((result, index) => {
-        const listItem = document.createElement('li');
-  
-        if (result.ip) {
-          listItem.textContent = `来源${index + 1}：${result.ip}`;
-        } else {
-          listItem.innerHTML = `
-            <span class="error">来源${index + 1}：获取失败</span>
-            <span class="error-detail">${result.error}</span>
-          `;
-        }
-  
-        ipListElement.appendChild(listItem);
-      });
+// API functions
+const fetchIpifyOrg = () => fetch('https://api.ipify.org?format=json')
+  .then(response => response.json())
+  .then(data => ({ name: 'IpifyOrg', ...data }))
+  .catch(error => ({ name: 'IpifyOrg', error }));
+
+const fetchIpifyOrg64 = () => fetch('https://api64.ipify.org?format=json')
+  .then(response => response.json())
+  .then(data => ({ name: 'IpifyOrg64', ...data }))
+  .catch(error => ({ name: 'IpifyOrg64', error }));
+
+// https://api.ip.sb/geoip
+// response {
+// "organization": "Ucloud Information Technology Hk Limited",
+// "longitude": 114.1759,
+// "city": "Hong Kong",
+// "timezone": "Asia/Hong_Kong",
+// "isp": "Ucloud Information Technology Hk Limited",
+// "offset": 28800,
+// "region": "Central and Western District",
+// "asn": 135377,
+// "asn_organization": "UCLOUD INFORMATION TECHNOLOGY HK LIMITED",
+// "country": "Hong Kong",
+// "ip": "45.249.246.129",
+// "latitude": 22.2842,
+// "continent_code": "AS",
+// "country_code": "HK",
+// "region_code": "HCW"
+// }
+const fetchIpSbGeoip = () => fetch('https://api.ip.sb/geoip')
+  .then(response => response.json())
+  .then(data => {
+    return data.ip ? { name: 'IpSbGeoip', ip: data.ip } : { name: 'IpSbGeoip', error: 'No IP found in response' };
+  })
+  .catch(error => ({ name: 'IpSbGeoip', error }));
+
+
+// New function from https://2023.ip138.com/
+// It returns HTML, and you should extract xxx.xxx.xxx.xxx from its response html
+const fetchIp138 = () => fetch('https://2023.ip138.com/')
+  .then(response => response.text())
+  .then(html => {
+    const match = html.match(/\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b/);
+    return match ? { name: 'Ip138', ip: match[0] } : { name: 'Ip138', error: 'No IP found in response' };
+  })
+  .catch(error => ({ name: 'Ip138', error }));
+
+const ipListElement = document.getElementById('ip-list');
+const loadingElement = document.getElementById('loading');
+
+// Page load event
+window.onload = function() {
+  // Show loading indicator
+  loadingElement.style.display = 'block';
+
+  // Use Promise.all to wait for all API responses
+  Promise.all([
+    fetchIpifyOrg(),
+    fetchIpifyOrg64(),
+    fetchIp138(),
+    fetchIpSbGeoip()
+  ])
+  .then(results => {
+    // Hide loading indicator
+    loadingElement.style.display = 'none';
+
+    // Create a table
+    const table = document.createElement('table');
+    const thead = document.createElement('thead');
+    const tbody = document.createElement('tbody');
+    const headerRow = document.createElement('tr');
+
+    // Define headers
+    ['Source', 'IP Address', 'Error'].forEach((headerText) => {
+      const th = document.createElement('th');
+      th.textContent = headerText;
+      headerRow.appendChild(th);
     });
-  };
-  
+
+    thead.appendChild(headerRow);
+    table.appendChild(thead);
+
+    // Populate table with results
+    results.forEach((result) => {
+      const row = document.createElement('tr');
+      const sourceCell = document.createElement('td');
+      const ipCell = document.createElement('td');
+      const errorCell = document.createElement('td');
+
+      sourceCell.textContent = result.name;
+      ipCell.textContent = result.ip || '';
+      errorCell.textContent = result.error || '';
+
+      row.appendChild(sourceCell);
+      row.appendChild(ipCell);
+      row.appendChild(errorCell);
+      tbody.appendChild(row);
+    });
+
+    table.appendChild(tbody);
+    ipListElement.appendChild(table);
+  });
+};
+
