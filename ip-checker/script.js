a6dcd634a5a83c10cecdd58bb6fa671f1591ab18
tyz <tangyouze@gmail.com>
2023-09-30 09:13:24 +0800

    multiple

--- ip-checker/script.js
+++ ip-checker/script.js
@@ -1,14 +1,41 @@
-document.getElementById('fetch-ip').addEventListener('click', function() {
-    // 这里以 https://api.ipify.org?format=json 为例，你可以替换为其他API
-    fetch('https://api.ipify.org?format=json')
+// API列表
+const apiList = [
+    'https://api.ipify.org?format=json',
+    'https://api64.ipify.org?format=json',
+    // 添加其他3个API
+  ];
+  
+  const ipListElement = document.getElementById('ip-list');
+  const loadingElement = document.getElementById('loading');
+  
+  // 页面加载时自动执行
+  window.onload = function() {
+    // 显示加载指示器
+    loadingElement.style.display = 'block';
+  
+    // 使用Promise.all等待所有API响应
+    Promise.all(apiList.map(url => fetch(url)
       .then(response => response.json())
-      .then(data => {
-        const ipAddress = data.ip;
-        document.getElementById('ip-address').innerText = `你的 IP 地址是：${ipAddress}`;
-      })
-      .catch(error => {
-        console.error('获取IP地址出错', error);
-        document.getElementById('ip-address').innerText = '获取IP地址失败';
+      .catch(error => ({ error }))
+    ))
+    .then(results => {
+      // 隐藏加载指示器
+      loadingElement.style.display = 'none';
+  
+      results.forEach((result, index) => {
+        const listItem = document.createElement('li');
+  
+        if (result.ip) {
+          listItem.textContent = `来源${index + 1}：${result.ip}`;
+        } else {
+          listItem.innerHTML = `
+            <span class="error">来源${index + 1}：获取失败</span>
+            <span class="error-detail">${result.error}</span>
+          `;
+        }
+  
+        ipListElement.appendChild(listItem);
       });
-  });
+    });
+  };
   
